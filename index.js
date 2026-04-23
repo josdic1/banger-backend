0fae3bc72145d81f850f206a61879775848749cd
josdic1 <joshuadicker@gmail.com>
2026-04-23 15:58:49 -0400

    fix cors for production

--- index.js
+++ index.js
@@ -10,7 +10,16 @@ console.log({
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://joshdicker.netlify.app",
+      "https://demberry.com",
+      "https://www.demberry.com",
+    ],
+  }),
+);
 app.use(express.json());
 
 app.use("/uploads", express.static("uploads"));
